57f95ab15afc8435b41b058f17e80b29805783a7
Bryan Medica <bryan.medica@epitech.eu>
2020-02-06 18:48:37 +0700

    Updates

---
+++ precache-manifest.61d2fb3c55f392309b8ce3f3032cd7b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7acd85abf431a570bcd359b8a3524db6",
+    "url": "/tictactoe.git/index.html"
+  },
+  {
+    "revision": "5e2f0f24bc167eaee58c",
+    "url": "/tictactoe.git/static/css/main.50d3536d.chunk.css"
+  },
+  {
+    "revision": "20f33026034870246543",
+    "url": "/tictactoe.git/static/js/2.82f37661.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tictactoe.git/static/js/2.82f37661.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e2f0f24bc167eaee58c",
+    "url": "/tictactoe.git/static/js/main.9edb0327.chunk.js"
+  },
+  {
+    "revision": "bbc123740a63c5e663ba",
+    "url": "/tictactoe.git/static/js/runtime-main.3587fed9.js"
+  }
+]);
