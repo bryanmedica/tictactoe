f537fb7fb60b39500b0831da03b2bd248b20f37f
Bryan Medica <bryan.medica@epitech.eu>
2020-02-06 18:49:37 +0700

    Updates

---
+++ precache-manifest.5b9a9ca671052360e6ed763f01c10d20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e2ec4cb042acaa8546809822464659",
+    "url": "/bryanmedica/tictactoe.git/index.html"
+  },
+  {
+    "revision": "5e2f0f24bc167eaee58c",
+    "url": "/bryanmedica/tictactoe.git/static/css/main.50d3536d.chunk.css"
+  },
+  {
+    "revision": "20f33026034870246543",
+    "url": "/bryanmedica/tictactoe.git/static/js/2.82f37661.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bryanmedica/tictactoe.git/static/js/2.82f37661.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e2f0f24bc167eaee58c",
+    "url": "/bryanmedica/tictactoe.git/static/js/main.9edb0327.chunk.js"
+  },
+  {
+    "revision": "c616f003f071b8587b1f",
+    "url": "/bryanmedica/tictactoe.git/static/js/runtime-main.fa1efcfa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tictactoe.git/precache-manifest.61d2fb3c55f392309b8ce3f3032cd7b7.js"
+  "/bryanmedica/tictactoe.git/precache-manifest.5b9a9ca671052360e6ed763f01c10d20.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tictactoe.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bryanmedica/tictactoe.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
